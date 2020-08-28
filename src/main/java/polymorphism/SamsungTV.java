a3160a850d60c6e57760c99dd329f1a7ccdf057f
cjc <w51w@naver.com>
2020-08-28 20:00:11 +0900

    의존성 관계 p65 - p 68

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -1,6 +1,9 @@
 package polymorphism;
 
 public class SamsungTV implements TV {
+	
+	private SonySpeaker speaker;
+	
 	public SamsungTV() {
 		System.out.println("[객체 생성]SamsungTV ");
 	}
@@ -14,10 +17,15 @@ public class SamsungTV implements TV {
 	}
 	@Override
 	public void volumeUp() {
-		System.out.println("삼성TV 소리 올림");
+		speaker = new SonySpeaker();
+		speaker.volumeUp();
 	}
 	@Override
 	public void volumeDown() {
-		System.out.println("삼성TV 소리 내림");
+		speaker = new SonySpeaker();
+		speaker.volumeDown();
 	}
 }
+//SonySpeaker 객체가 쓸데 없이 두 개나 생성되는 것
+//다른 스피커로 교체시 소스코드를 모두 수정해야 됨
+//스프링은 이 문제를 의존성 주입을 이용하여  해결한다.
---
+++ src/main/java/polymorphism/SonySpeaker.java
@@ -0,0 +1,13 @@
+package polymorphism;
+
+public class SonySpeaker {
+	public SonySpeaker() {
+		System.out.println("[객체 생성]소니스피커");
+	}
+	public void volumeUp() {
+		System.out.println("소니스피커 소리 올린다");
+	}
+	public void volumeDown() {
+		System.out.println("소니스피커 소리 내린다");
+	}
+}
