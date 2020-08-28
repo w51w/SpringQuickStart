05358b66dd4dcf4062205e5f79c64eaed2a0a928
cjc <w51w@naver.com>
2020-08-28 12:26:52 +0900

    디자인 패턴 이용하기 p44 - p47

--- src/main/java/polymorphism/TVUser.java
+++ src/main/java/polymorphism/TVUser.java
@@ -3,7 +3,12 @@ package polymorphism;
 public class TVUser {
 
 	public static void main(String[] args) {
-		TV tv = new LgTV();
+		BeanFactory factory = new BeanFactory();
+		TV tv = (TV)factory.getBean(args[0]);
+		
+		//프로그램 실행 시 명령행 매개변수를 전달하면 바뀜
+		//Run as> Run Configuration > argument에 lg 또는 samsung 을 입력하고 Run해보자
+		
 		tv.powerOn();
 		tv.volumeUp();
 		tv.volumeDown();
