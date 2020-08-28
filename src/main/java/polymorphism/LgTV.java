d7f0ca6e329a5b468ef6f3600e21565d7758646f
cjc <w51w@naver.com>
2020-08-29 00:15:44 +0900

    어노테이션 , @componet p93 - p97

--- src/main/java/polymorphism/LgTV.java
+++ src/main/java/polymorphism/LgTV.java
@@ -1,5 +1,11 @@
 package polymorphism;
 
+import org.springframework.stereotype.Component;
+
+//클라이언트가 스프링 컨테이너가 생성한 객체를 요청하려면 
+//아이디나 이름이 반드시 설정되어 있어야 한다 
+//없다면 그냥 객체가 메모리에 생성만 된다
+@Component("tv") 	//클래스 위 선언, 객체 생성
 public class LgTV implements TV{
 	public LgTV() {
 		System.out.println("[객체 생성]LgTV");
