0e4da3f42450cd5e32e727c88198400534f76aa4
cjc <w51w@naver.com>
2020-08-28 12:07:50 +0900

    결합도가 높은 프로그램 p39 - p41

---
+++ src/main/java/polymorphism/LgTV.java
@@ -0,0 +1,16 @@
+package polymorphism;
+
+public class LgTV {
+	public void turnOn() {
+		System.out.println("LgTV 전원 켬");
+	}
+	public void turnOff() {
+		System.out.println("LgTV 전원 끔");
+	}
+	public void soundUp() {
+		System.out.println("LgTV 소리 올림");
+	}
+	public void soundDown() {
+		System.out.println("LgTV 소리 내림");
+	}
+}
---
+++ src/main/java/polymorphism/SamsungTV.java
@@ -0,0 +1,16 @@
+package polymorphism;
+
+public class SamsungTV {
+	public void powerOn() {
+		System.out.println("伙己TV 傈盔 脑");
+	}
+	public void powerOff() {
+		System.out.println("伙己TV 傈盔 阐");
+	}
+	public void volumeUp() {
+		System.out.println("伙己TV 家府 棵覆");
+	}
+	public void volumeDown() {
+		System.out.println("伙己TV 家府 郴覆");
+	}
+}
