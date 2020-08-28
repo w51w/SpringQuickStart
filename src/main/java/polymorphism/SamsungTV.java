fd4a099dfd77c2d800519bb35b89a26ac6a331e2
cjc <w51w@naver.com>
2020-08-28 20:12:56 +0900

    생성자 인젝션 p69 - p75

--- src/main/java/polymorphism/SamsungTV.java
+++ src/main/java/polymorphism/SamsungTV.java
@@ -5,7 +5,11 @@ public class SamsungTV implements TV {
 	private SonySpeaker speaker;
 	
 	public SamsungTV() {
-		System.out.println("[객체 생성]SamsungTV ");
+		System.out.println("[객체 생성]SamsungTV(1) ");
+	}
+	public SamsungTV(SonySpeaker speaker) {
+		System.out.println("[객체 생성]SamsungTV(2) ");
+		this.speaker = speaker;
 	}
 	@Override
 	public void powerOn() {
@@ -17,15 +21,10 @@ public class SamsungTV implements TV {
 	}
 	@Override
 	public void volumeUp() {
-		speaker = new SonySpeaker();
 		speaker.volumeUp();
 	}
 	@Override
 	public void volumeDown() {
-		speaker = new SonySpeaker();
 		speaker.volumeDown();
 	}
 }
-//SonySpeaker 객체가 쓸데 없이 두 개나 생성되는 것
-//다른 스피커로 교체시 소스코드를 모두 수정해야 됨
-//스프링은 이 문제를 의존성 주입을 이용하여  해결한다.
