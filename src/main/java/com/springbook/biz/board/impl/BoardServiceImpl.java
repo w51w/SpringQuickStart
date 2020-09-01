07727d4253c15486fbfbb8245c75c952e87e5acc
cjc <w51w@naver.com>
2020-09-01 12:31:31 +0900

    @afterThrowing p202 - p203

--- src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -15,6 +15,9 @@ public class BoardServiceImpl implements BoardService{
 	
 	@Override
 	public void insertBoard(BoardDTO vo) {
+		if(vo.getSeq() == 0) {
+			throw new IllegalArgumentException("0번 글은 등록 할 수 없습니다.");
+		}
 		boardDAO.insertBoard(vo);
 	}
 
--- src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -1,17 +1,7 @@
 package com.springbook.biz.common;
 
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Service;
-
-@Service
-@Aspect
 public class AfterReturningAdvice {
-	@Pointcut("execution(* com.springbook.biz..*Impl.get*(..))")
-	public void getPointcut() {}
-	
-	@AfterReturning(pointcut="getPointcut()", returning="returnObj")
+
 	public void afterLog() {
 		System.out.println("[사후 처리] 비즈니스 로직 수행 후 동작");
 	}
