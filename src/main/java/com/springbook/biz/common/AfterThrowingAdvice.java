59a60cd5cd54ce18b9ae23c2c822463b9d392b69
cjc <w51w@naver.com>
2020-09-01 12:43:03 +0900

    @around p204-p206

--- src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
+++ src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
@@ -1,18 +1,7 @@
 package com.springbook.biz.common;
 
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Service;
-
-@Service
-@Aspect
 public class AfterThrowingAdvice {
-	
-	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
-	public void allPointcut() {}
-	
-	@AfterThrowing(pointcut="allPointcut()", throwing="exceptObj")
+
 	public void exceptionLog() {
 		System.out.println("[예외 처리] 비즈니스 로직 수행 중 예외 발생");
 	}
--- src/main/java/com/springbook/biz/common/AroundAdvice.java
+++ src/main/java/com/springbook/biz/common/AroundAdvice.java
@@ -1,8 +1,18 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
+@Service
+@Aspect
 public class AroundAdvice {
+	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+	public void allPointcut() {}
+	
+	@Around("allPointcut()")
 	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable{
 		System.out.println("[BEFORE] 비즈니스 메소드 수행 전에 처리할 내용...");
 		Object returnobj = pjp.proceed();
