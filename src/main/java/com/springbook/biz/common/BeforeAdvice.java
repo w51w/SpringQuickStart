bf0a8402d0c35e9495f032cf072de96a7b30181f
cjc <w51w@naver.com>
2020-09-01 12:25:11 +0900

    @afterReturning p200-p201

--- src/main/java/com/springbook/biz/common/BeforeAdvice.java
+++ src/main/java/com/springbook/biz/common/BeforeAdvice.java
@@ -1,17 +1,6 @@
 package com.springbook.biz.common;
 
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.stereotype.Service;
-
-@Service
-@Aspect
 public class BeforeAdvice {
-	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
-	public void allPointcut() {}
-	
-	@Before("allPointcut()")
 	public void beforeLog() {
 		System.out.println("[사전 처리] 비즈니스 로직 수행 전 동작");
 	}
