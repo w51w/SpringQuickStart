e0ae84f44aed69c2f3fe063ff73373f9fede2abe
cjc <w51w@naver.com>
2020-09-01 14:06:13 +0900

    외부 Pointcut 참조하기 p206 - p208

--- src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
+++ src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -1,7 +1,14 @@
 package com.springbook.biz.common;
 
-public class AfterReturningAdvice {
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Service;
 
+@Service
+@Aspect
+public class AfterReturningAdvice {
+	
+	@AfterReturning(pointcut="PointcutCommon.getPointcut()", returning="returnObj")
 	public void afterLog() {
 		System.out.println("[사후 처리] 비즈니스 로직 수행 후 동작");
 	}
--- src/main/java/com/springbook/biz/common/BeforeAdvice.java
+++ src/main/java/com/springbook/biz/common/BeforeAdvice.java
@@ -1,6 +1,14 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Service;
+
+@Service
+@Aspect
 public class BeforeAdvice {
+	
+	@Before("PointcutCommon.allPointcut()")
 	public void beforeLog() {
 		System.out.println("[사전 처리] 비즈니스 로직 수행 전 동작");
 	}
