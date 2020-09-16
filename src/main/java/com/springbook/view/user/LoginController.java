0ea0db7bb4600d4db1c40f3e4df5f0ec00add8aa
cjc <w51w@naver.com>
2020-09-16 23:12:42 +0900

    @ModelAttribute p377~p378

--- src/main/java/com/springbook/view/user/LoginController.java
+++ src/main/java/com/springbook/view/user/LoginController.java
@@ -1,6 +1,7 @@
 package com.springbook.view.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,7 +11,7 @@ import com.springbook.biz.user.impl.UserDAO;
 @Controller
 public class LoginController  {
 	@RequestMapping(value="/login.do", method=RequestMethod.GET)
-	public String loginView(UserDTO vo) {
+	public String loginView(@ModelAttribute("user") UserDTO vo) {
 		System.out.println("로그인 화면으로 이동");
 		vo.setId("test");
 		vo.setPassword("test123");
