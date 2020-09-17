5bb6a4c68ea4e92f1446707f7330c4389477b03a
cjc <w51w@naver.com>
2020-09-17 10:07:46 +0900

    @RequestParam p382 - p384

--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -3,6 +3,7 @@ package com.springbook.view.board;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springbook.biz.board.BoardDTO;
@@ -32,8 +33,15 @@ public class BoardController {
 		return "getBoard.jsp";
 	}
 	@RequestMapping("/getBoardList.do")
-	public String getBoardList(BoardDTO vo , BoardDAO boardDAO, Model model) {
-		model.addAttribute("boardList", boardDAO.getBoardList(vo));
+	public String getBoardList(
+			@RequestParam(value="searchCondition", defaultValue = "TITLE", required = false)
+			//required = false 생략 안함.
+			//사용하기 싫다면 DTO에 변수 추가하고 겟셋 메소드 써줘도 무방하다.
+			String condition,
+			@RequestParam(value="searchKeyword", defaultValue = "", required = false)
+			String keyword, 
+			BoardDTO vo , BoardDAO boardDAO, Model model) {
+//		model.addAttribute("boardList", boardDAO.getBoardList(vo));
 		return "getBoardList.jsp";
 	}
 }
--- src/main/webapp/getBoardList.jsp
+++ src/main/webapp/getBoardList.jsp
@@ -15,6 +15,7 @@
 <h3>${userName }님 환영합니다...<a href="logout.do">Log-out</a></h3>
 
 <!-- 검색시작 -->
+
 <form action="getBoardList.jsp" method="post">
 	<table border="1" cellpadding="0" cellspacing="0" width="700">
 		<tr>
