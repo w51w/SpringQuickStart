5a664a265e332664d2b62ab213c63e7bc46018b3
cjc <w51w@naver.com>
2020-09-17 23:22:41 +0900

    @SessionAttributes p388 - p393

--- src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -20,7 +20,7 @@ public class BoardDAO  {
 	
 	private final String BOARD_INSERT = "insert into board(title, writer, content) values(?,?,?)";
 	//seq Ä®·³ autoincrement
-	private final String BOARD_UPDATE = "update board set title=?, content=? where seq=?";
+	private final String BOARD_UPDATE = "update board set title=?,writer=?, content=? where seq=?";
 	private final String BOARD_DELETE = "delete from board where seq=?";
 	private final String BOARD_GET = "select * from board where seq=?";
 	private final String BOARD_LIST = "select * from board order by seq desc";
@@ -50,8 +50,9 @@ public class BoardDAO  {
 			conn = JDBCUtil.getConnection();
 			pstmt = conn.prepareStatement(BOARD_UPDATE);
 			pstmt.setString(1, vo.getTitle());
-			pstmt.setString(2, vo.getContent());
-			pstmt.setInt(3, vo.getSeq());
+			pstmt.setString(2, vo.getWriter());
+			pstmt.setString(3, vo.getContent());
+			pstmt.setInt(4, vo.getSeq());
 			pstmt.executeUpdate();
 		}
 		catch(Exception e) {
--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -7,11 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.springbook.biz.board.BoardDTO;
 import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
+@SessionAttributes("board") 	//Model에 "board"라는 이름으로 저장되는 데이터가 있다면
+								//그 데이터를 세션(HttpSession)에도 자동 저장하라는 설정
 public class BoardController {
 	//검색 조건 목록 설정
 	//아래의 어노테이션은 View(jsp)에서 사용할 데이터를 설정하는 용도로 사용할 수도 있다.
@@ -29,7 +32,10 @@ public class BoardController {
 		return "getBoardList.do";
 	}
 	@RequestMapping("/updateBoard.do")
-	public String updateBoard(BoardDTO vo, BoardDAO boardDAO) {		
+	//스프링 컨테이너는 우선@ModelAttribute("board") 설정을 해석하여 board라는 이름으로 저장된 데이터가 있는 지를 확인한다.
+	//있다면 세션에서 꺼내 매개변수로 선언된 vo 변수에 할당 그리고 사용자가 입력한 파라미터를 vo 객체에 할당 그리고 다시 세션 유지
+	public String updateBoard(@ModelAttribute("board") BoardDTO vo, BoardDAO boardDAO) {	
+		System.out.println(vo.toString());
 		boardDAO.updateBoard(vo);
 		return "getBoardList.do";
 	}
@@ -40,6 +46,7 @@ public class BoardController {
 	}
 	@RequestMapping("/getBoard.do")
 	public String getBoard(BoardDTO vo , BoardDAO boardDAO, Model model) {
+		System.out.println(vo.getSeq());
 		model.addAttribute("board", boardDAO.getBoard(vo));
 		return "getBoard.jsp";
 	}
