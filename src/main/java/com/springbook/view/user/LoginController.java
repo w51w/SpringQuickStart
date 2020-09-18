4285959516bacc8e9de32577a7f921cc7eee4082
cjc <w51w@naver.com>
2020-09-18 21:42:30 +0900

    어노테이션 예외처리 p427- p432

--- src/main/java/com/springbook/view/user/LoginController.java
+++ src/main/java/com/springbook/view/user/LoginController.java
@@ -22,6 +22,9 @@ public class LoginController  {
 	
 	@RequestMapping(value="/login.do", method = RequestMethod.POST)
 	public String login(UserDTO vo, UserDAO userDAO, HttpSession session) {	
+		if(vo.getId() == null || vo.getId().equals("")) {
+			throw new IllegalArgumentException("아이디는 반드시 입력해야 합니다.");
+		}
 		UserDTO user = userDAO.getUser(vo);
 		if(user != null) {
 			session.setAttribute("userName", user.getName()); //세션에 저장된 사용자를 
